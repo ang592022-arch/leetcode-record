f03fd498ff2c819d707efc596dacd23d6445d729
ang592022-arch <ang592022@gmail.com>
2026-08-15 16:01:34 +0800

    Time: 4 ms (38.57%), Space: 17.7 MB (54.2%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+    unordered_map<int,int>map;
+      for(int num:nums){
+        map[num]++;
+      }
+      priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+      for(auto it:map){
+        pq.push({it.second,it.first});
+      
+      if(pq.size()>k){
+        pq.pop();
+      }}
+      vector<int>result;
+      while(!pq.empty()){
+        result.push_back(pq.top().second);
+        pq.pop();
+      }
+      return result;
+    }
+
+};
